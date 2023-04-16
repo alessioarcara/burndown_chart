41123a7dabd27fd7ca03f16df146cca4d5b96446
alessioarcara <Spammer123@>
2023-04-16 09:09:43 +0200

    improved code readability, error handling and organization in burndown_chart.py

--- burndown_chart.py
+++ burndown_chart.py
@@ -1,79 +1,101 @@
-from github import Github
-from datetime import date
-import matplotlib.pyplot as plt
-import numpy as np
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from github import Github, GithubException
+from datetime import date
 
-if len(sys.argv) < 7:
-    print("Errore: devi passare come argomenti da riga di comando:")
-    print("- GitHub token -> esempio: validToken")
-    print("- GitHub user -> esempio: validUser")
-    print("- GitHub repository -> esempio: validRepository")
-    print("- Numero della iterazione dello sprint -> esempio: 1,2,3, ...")
-    print("- Numero dei giorni -> esempio: 6,7,8, ...")
-    print("- Mostra grafico per giorni successivi ad oggi -> esempio: Mostra\n")
-    print("Esempio utilizzo:")
-    print("> python burndownchart.py validToken validUser valid 3 7 Mostra")
+
+def print_usage_and_exit():
+    print("Uso:")
+    print("python burndownchart.py <token> <user> <repository> <sprint> <days> <show>\n")
+    print("Parametri:")
+    print("<token> - Il tuo token di accesso personale GitHub.")
+    print("<user> - Il nome utente del proprietario del repository.")
+    print("<repository> - Il nome del repository.")
+    print("<sprint> - Il numero dello sprint (un numero intero positivo).")
+    print("<days> - Il numero di giorni per lo sprint (un numero intero positivo).")
+    print("<show> - 'Show' per mostrare i giorni successivi ad oggi nel grafico.")
+    print("\nEsempio:")
+    print("> python burndownchart.py validToken validUser validRepository 3 7 Show")
     sys.exit()
 
 
-def check_positive_int(param):
+def check_positive_integer(param):
     try:
-        param = int(param)
+        value = int(param)
+        if value < 0:
+            raise ValueError
     except ValueError:
-        print("Errore: il parametro deve essere un numero intero.")
+        print(f"Errore: {param} deve essere un numero intero positivo.")
         sys.exit()
-
-    if param < 0:
-        print("Errore: il parametro deve essere un numero positivo.")
-        sys.exit()
-    return param
+    return value
 
 
-TOKEN = sys.argv[1]
-OWNER = sys.argv[2]
-REPOSITORY = sys.argv[3]
-SPRINT = check_positive_int(sys.argv[4])
-DAYS = check_positive_int(sys.argv[5])
-MOSTRA = sys.argv[6] == "Mostra"
-
-
-def get_actual(issues):
-    # init
-    actual = np.full(DAYS, issues.totalCount, dtype=float)
-    bins = np.zeros(DAYS)
+def calculate_actual_and_bins(issues, days, show):
+    actual = np.full(days, issues.totalCount, dtype=float)
+    bins = np.zeros(days)
     closed_issues = [issue for issue in issues if issue.closed_at is not None]
     closed_issues.sort(key=lambda x: x.closed_at, reverse=False)
 
     for closed_issue in closed_issues:
         day = closed_issue.closed_at.isoweekday()
-        actual[day] -= 1
-        actual[day + 1:] = actual[day]
+        actual[day:] -= 1
         bins[day] += 1
 
-    if not MOSTRA:
-        actual[date.today().isoweekday():] = actual[date.today().isoweekday():] + np.NAN
+    if not show:
+        actual[date.today().isoweekday():] = np.NAN
     return actual, bins
 
 
-g = Github(TOKEN)
-repo = g.get_repo(f"{OWNER}/{REPOSITORY}")
-issues = repo.get_issues(state='all', labels=['task', f"s0{SPRINT}"])
+def parse_arguments():
+    if len(sys.argv) < 7:
+        print_usage_and_exit()
 
-fig, ax = plt.subplots()
+    args = {
+        "token": sys.argv[1],
+        "owner": sys.argv[2],
+        "repository": sys.argv[3],
+        "sprint": check_positive_integer(sys.argv[4]),
+        "days": check_positive_integer(sys.argv[5]),
+        "show": sys.argv[6] == "Show",
+    }
 
-x = np.arange(DAYS)
-actual, bins = get_actual(issues)
-ideal = [x - i * (issues.totalCount / (DAYS - 1))
-         for i, x in enumerate(np.full(DAYS, issues.totalCount))]
-ax.plot(x, ideal, linestyle='dashed', color='green', label="Ideal")
-ax.step(x, actual, where='post', label="Actual")
-ax.bar(x, bins, width=0.2, color='orange', label="Completed tasks")
+    return args
 
-ax.set_title(f"Burn down chart for Sprint {SPRINT}")
-ax.set_ylabel("Remaining and completed tasks")
-ax.legend(loc="upper right")
 
-plt.xticks(x, [f"Day\n{ix}" for ix in x])
-plt.savefig("burndownchart.png")
-plt.show()
+def create_burndown_chart(days, sprint, actual, ideal, bins):
+    fig, ax = plt.subplots()
+    x = np.arange(days)
+
+    ax.plot(x, ideal, linestyle="dashed", color="green", label="Ideal")
+    ax.step(x, actual, where="post", label="Actual")
+    ax.bar(x, bins, width=0.2, color="orange", label="Completed tasks")
+
+    ax.set_title(f"Burn down chart for Sprint {sprint}")
+    ax.set_ylabel("Remaining and completed tasks")
+    ax.legend(loc="upper right")
+
+    plt.xticks(x, [f"Day\n{ix}" for ix in x])
+    plt.savefig("burndownchart.png")
+    plt.show()
+
+
+def main():
+    args = parse_arguments()
+
+    try:
+        github_api = Github(args["token"])
+        repo = github_api.get_repo(f"{args['owner']}/{args['repository']}")
+    except GithubException as e:
+        print(f"Errore: {e.data.get('message'), 'Token non valido o problema di accesso al repository.'}")
+        sys.exit()
+
+    issues = repo.get_issues(state='all', labels=['task', f"s0{args['sprint']}"])
+    actual, bins = calculate_actual_and_bins(issues, args['days'], args['show'])
+    ideal = np.linspace(issues.totalCount, 0, args['days'])
+
+    create_burndown_chart(args['days'], args['sprint'], actual, ideal, bins)
+
+
+if __name__ == "__main__":
+    main()
